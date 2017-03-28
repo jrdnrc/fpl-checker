1d9f81c1db14d33e8412ce58461e820621851193
Jordan Crocker <jordan@jcrocker.uk>
2017-03-28 22:15:25 +0100

    [Dependencies] Install dusk and image reader

---
+++ tests/DuskTestCase.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace JrdnRc\FplChecker\Tests;
+
+use Laravel\Dusk\TestCase as BaseTestCase;
+use Facebook\WebDriver\Remote\RemoteWebDriver;
+use Facebook\WebDriver\Remote\DesiredCapabilities;
+
+abstract class DuskTestCase extends BaseTestCase
+{
+    use CreatesApplication;
+
+    /**
+     * Prepare for Dusk test execution.
+     *
+     * @beforeClass
+     * @return void
+     */
+    public static function prepare()
+    {
+        static::startChromeDriver();
+    }
+
+    /**
+     * Create the RemoteWebDriver instance.
+     *
+     * @return \Facebook\WebDriver\Remote\RemoteWebDriver
+     */
+    protected function driver()
+    {
+        return RemoteWebDriver::create(
+            'http://localhost:9515',
+            DesiredCapabilities::htmlUnit()->setJavascriptEnabled(false),
+            30000000
+        );
+    }
+}
